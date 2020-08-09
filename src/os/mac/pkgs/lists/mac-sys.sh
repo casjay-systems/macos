89f3cff571297e95826361bd3a9eebf19a8cbfda
Jason Hempstead <git-admin@casjaysdev.com>
2020-08-09 14:28:03 -0400

    🦈🏠 Working on fixes for MacOS and Win 🏠🦈

--- src/os/mac/pkgs/lists/mac-sys.sh
+++ src/os/mac/pkgs/lists/mac-sys.sh
@@ -5,6 +5,7 @@ brew install -f git fortune cowsay neofetch coreutils fish bash zsh bash-complet
 brew install -f mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions
 brew install -f ruby php perl nodejs golang nvm youtube-dl direnv wget curl iproute2mac powerline-go
 brew install -f nano neovim macvim emacs thefuck
+brew install -f byobu nethogs iftop iperf jq dialog links html2text dict speedtest-cli
 brew cask install -f font-ubuntu font-powerline-symbols font-powerline-symbols visual-studio-code firefox atom
 brew cask install -f powershell libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird
 brew cask install -f authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass
