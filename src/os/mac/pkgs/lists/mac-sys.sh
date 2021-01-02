56b2379221344af2f97826e1620e63bfc92650d3
casjay <git-admin@casjaysdev.com>
2021-01-02 03:24:07 -0500

    Update mac-sys.sh

--- src/os/mac/pkgs/lists/mac-sys.sh
+++ src/os/mac/pkgs/lists/mac-sys.sh
@@ -4,10 +4,9 @@ brew tap homebrew/cask-fonts
 brew install -f git fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync
 brew install -f mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions
 brew install -f ruby php perl nodejs golang nvm youtube-dl direnv wget curl iproute2mac powerline-go
-brew install -f nano neovim macvim emacs thefuck
+brew install -f nano neovim macvim emacs thefuck fnm
 brew install -f byobu nethogs iftop iperf jq dialog links html2text dict speedtest-cli
-brew cask install -f font-ubuntu font-powerline-symbols font-powerline-symbols visual-studio-code firefox atom
-brew cask install -f powershell libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird
-brew cask install -f authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass
-brew cask install -f the-unarchiver obs skype spectacle alfred
-brew install Schniz/tap/fnm
+brew install --cask -f font-ubuntu font-powerline-symbols font-powerline-symbols visual-studio-code firefox atom
+brew install --cask -f powershell libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird
+brew install --cask -f authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass
+brew install --cask -f the-unarchiver obs skype spectacle alfred
