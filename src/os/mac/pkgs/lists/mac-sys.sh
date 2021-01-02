836b889b7a794f6243b3782705e994b38701ebb8
casjay <git-admin@casjaysdev.com>
2021-01-02 04:02:25 -0500

    Update mac-sys.sh

--- src/os/mac/pkgs/lists/mac-sys.sh
+++ src/os/mac/pkgs/lists/mac-sys.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-brew tap homebrew/cask-fonts
-brew install -f git svn fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync
-brew install -f mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions
-brew install -f ruby php perl nodejs golang nvm youtube-dl direnv wget curl iproute2mac powerline-go
-brew install -f nano neovim macvim emacs thefuck fnm
-brew install -f byobu nethogs iftop iperf jq dialog links html2text dict speedtest-cli
-brew install --cask -f font-ubuntu font-powerline-symbols font-powerline-symbols visual-studio-code firefox atom
-brew install --cask -f powershell libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird
-brew install --cask -f authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass
-brew install --cask -f the-unarchiver obs skype spectacle alfred
+# Define taps
+TAPS="homebrew/cask-fonts "
+
+# Define brew apps
+BREW="git svn fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync "
+BREW+="nano neovim macvim emacs thefuck fnm byobu nethogs iftop iperf jq dialog links html2text "
+BREW+="mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions speedtest-cli"
+BREW+="ruby php perl nodejs golang nvm youtube-dl direnv wget curl iproute2mac powerline-go dict "
+
+#Define brew casks apps
+CASKS="font-ubuntu font-powerline-symbols font-powerline-symbols visual-studio-code firefox atom obs "
+CASKS+="powershell libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird skype "
+CASKS+="authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass spectacle alfred the-unarchiver "
+
+# install
+for tap in $TAPS; do
+  execute "brew tap $tap" "Setting up $tap"
+done
+
+for brew in $BREW; do
+  execute "brew install -f $brew" "Setting up $brew"
+done
+
+for cask in $CASKS; do
+  execute "brew install --cask -f $cask" "Setting up $cask"  
+done
