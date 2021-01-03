7b2b4098bada74e2057495b18945dca701b573c8
Jason <jason@casjay.net>
2021-01-03 05:07:52 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- src/os/mac/pkgs/lists/mac-sys.sh
+++ src/os/mac/pkgs/lists/mac-sys.sh
@@ -7,35 +7,48 @@ srcdir="$(cd ../../.. && pwd)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_packages() {
-
+brew_tap_packages() {
   # Define taps
-  TAPS="homebrew/cask-fonts"
-
-  # Define brew apps
-  BREW="git svn fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync "
-  BREW+="nano neovim macvim emacs thefuck fnm byobu nethogs iftop iperf jq dialog links html2text "
-  BREW+="mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions speedtest-cli "
-  BREW+="ruby php perl nvm youtube-dl direnv wget curl iproute2mac powerline-go dict "
-
-  #Define brew casks apps
-  CASKS="font-ubuntu font-powerline-symbols visual-studio-code firefox atom obs powershell "
-  CASKS+="libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird skype "
-  CASKS+="authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass spectacle alfred the-unarchiver "
+  local TAPS="homebrew/cask-fonts"
 
   # install
-  execute "brew tap ${TAPS}" "Setting up taps"
+  for tap in ${TAPS}; do
+    execute "brew tap ${tap}" "Setting up taps"
+  done
+}
 
-  execute "brew install -f ${BREW}" "Setting up brew packages"
+brew_app_packages() {
+  # Define brew apps
+  local BREWS="git svn fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync "
+  local BREWS+="nano neovim macvim emacs thefuck fnm byobu nethogs iftop iperf jq dialog links html2text "
+  local BREWS+="mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions speedtest-cli "
+  local BREWS+="ruby php perl node golang nvm youtube-dl direnv wget curl iproute2mac powerline-go dict "
 
+  # install
+  #for brew in ${BREWS}; do
+  execute "brew install -f ${BREWS}" "Setting up brew apps"
+  #done
+}
+
+brew_casks_packages() {
+  #Define brew casks apps
+  local CASKS="font-ubuntu font-powerline-symbols visual-studio-code firefox atom obs powershell "
+  local CASKS+="libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird skype "
+  local CASKS+="authy darktable nextcloud brackets iterm2 terminology vlc postman lastpass spectacle alfred the-unarchiver "
+
+  # install
+  #for cask in ${CASKS}; do
   execute "brew install --cask -f ${CASKS}" "Setting up brew casks"
+  #done
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
 
-  brew_packages
+  brew_tap_packages
+  brew_app_packages
+  brew_casks_packages
 
 }
 
