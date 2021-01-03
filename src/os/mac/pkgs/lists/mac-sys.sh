93f9c6b10ae139c7cd1fbddce186e735ec3d2431
Jason <jason@casjay.net>
2021-01-03 18:42:42 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- src/os/mac/pkgs/lists/mac-sys.sh
+++ src/os/mac/pkgs/lists/mac-sys.sh
@@ -9,7 +9,9 @@ srcdir="$(cd ../../.. && pwd)"
 
 brew_tap_packages() {
   installcmd() {
-    execute "brew tap ${tap}" "Setting up ${tap}       "
+    for tap in ${@}; do
+      execute "brew tap ${tap}" "Setting up ${tap}       "
+    done
   }
 
   # Define taps
@@ -20,46 +22,55 @@ brew_tap_packages() {
 
   # install
   if [ -n "$TAPS" ]; then
-    for tap in ${TAPS}; do
-      execute "installcmd ${tap}" "Setting up taps       "
-    done
+    execute "installcmd ${TAPS}" "Setting up taps       "
     unset TAPS
   fi
 }
 
 brew_app_packages() {
   installcmd() {
-    execute "brew install -f ${brew}" "Setting up ${brew}       "
+    for brew in ${@}; do
+      execute "brew install -f ${brew}" "Setting up ${brew}       "
+    done
   }
 
   # Define brew apps
-  local BREWS+="font-ubuntu font-powerline-symbols font-fira-code font-hack-nerd-font "
-  local BREWS="git svn fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync typora "
-  local BREWS+="nano neovim macvim emacs thefuck fnm byobu nethogs iftop iperf jq dialog links html2text "
-  local BREWS+="mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions speedtest-cli "
-  local BREWS+="ruby php perl node golang nvm youtube-dl direnv wget curl iproute2mac powerline-go dict "
-
+  if [ -n "TRAVIS" ]; then
+    local BREWS="git svn fortune "
+  else
+    local BREWS="font-ubuntu font-powerline-symbols font-fira-code font-hack-nerd-font "
+    local BREWS+="git svn fortune cowsay neofetch coreutils fish bash zsh bash-completion@2 rsync typora "
+    local BREWS+="nano neovim macvim emacs thefuck fnm byobu nethogs iftop iperf jq dialog links html2text "
+    local BREWS+="mpd ncmpcpp newsboat pass editorconfig tmux screen hub zsh fish zsh-completions speedtest-cli "
+    local BREWS+="ruby php perl node golang nvm youtube-dl direnv wget curl iproute2mac powerline-go dict "
+  fi
   # install
-  for brew in ${BREWS[@]}; do
-    execute "installcmd ${brew}" "Setting up apps       "
-  done
+  if [ -n "$CASKS" ]; then
+    execute "installcmd ${BREWS}" "Setting up apps       "
+  fi
   unset BREWS
 }
 
 brew_casks_packages() {
   installcmd() {
-    execute "brew install --cask -f ${cask}" "Setting up ${cask}       "
+    for cask in ${@}; do
+      execute "brew install --cask -f ${cask}" "Setting up ${cask}       "
+    done
   }
 
   #Define brew casks apps
-  local CASKS="visual-studio-code firefox atom obs powershell "
-  local CASKS+="libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird skype "
-  local CASKS+="authy darktable nextcloud brackets iterm2 terminology vlc insomnia lastpass spectacle alfred the-unarchiver "
+  if [ -n "$TRAVIS" ]; then
+    local CASKS="visual-studio-code "
+  else
+    local CASKS="visual-studio-code firefox atom obs powershell "
+    local CASKS+="libreoffice transmission gpg-suite opera brave-browser tor-browser thunderbird skype "
+    local CASKS+="authy darktable nextcloud brackets iterm2 terminology vlc insomnia lastpass spectacle alfred the-unarchiver "
+  fi
 
   # install
-  for cask in ${CASKS[@]}; do
-    execute "installcmd ${cask}" "Setting up casks       "
-  done
+  if [ -n "$CASKS" ]; then
+    execute "installcmd ${CASKS}" "Setting up casks       "
+  fi
   unset CASKS
 }
 
